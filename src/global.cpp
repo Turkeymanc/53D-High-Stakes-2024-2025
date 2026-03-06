defe393045d62bdb20043ed2928695f290f94f72
Turkeymanc <87395820+Turkeymanc@users.noreply.github.com>
2026-03-06 18:33:31 -0500

    Update global.cpp

--- src/global.cpp
+++ src/global.cpp
@@ -4,7 +4,7 @@
 pros::Controller Controller(pros::E_CONTROLLER_MASTER);
 
 pros::MotorGroup left_mg({-2, 1}, pros::v5::MotorGears::blue);
-pros::MotorGroup right_mg({-11, -12}, pros::v5::MotorGears::blue);
+pros::MotorGroup right_mg({-11, 12}, pros::v5::MotorGears::blue);
 
 pros::IMU imu(8);
 pros::Rotation LadybrownRotation(18);
